85a9ecd6ef40796675d385c9eabdc1a58f184b19
olexii-d <tokyojapanjapan@gmail.com>
2026-02-06 16:54:34 +0900

    done3

---
+++ task3.py
@@ -0,0 +1,26 @@
+import re
+
+
+def normalize_phone(phone_number: str) -> str:
+    digits = re.sub(r"\D", "", phone_number)
+
+    if digits.startswith("380"):
+        return f"+{digits}"
+    return f"+38{digits}"
+
+
+if __name__ == "__main__":
+    raw_numbers = [
+        "067\t123 4567",
+        "(095) 234-5678\n",
+        "+380 44 123 4567",
+        "380501234567",
+        "    +38(050)123-32-34",
+        "     0503451234",
+        "(050)8889900",
+        "38050-111-22-22",
+        "38050 111 22 11   ",
+    ]
+
+    sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+    print(sanitized_numbers)
